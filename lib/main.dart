234ab7414398186f25cb4a4f7ccb4c2cb2069104
Martin Kollar <mato.kollar2004@gmail.com>
2025-01-14 20:17:07 +0100

    Reinitialized project with firebase configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:bonded/screens/wrapper.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
 import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(
+
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
